4c9db24f962d39544bfdf8ad27848a9e03f4c235
Gregory Worrall <gregjworrall@gmail.com>
2019-01-28 11:48:23 +0000

    Initialised

---
+++ index.js
@@ -0,0 +1,60 @@
+const corpus = require('./abuse.json').words;
+
+class Filter {
+  constructor(options = {}) {
+    Object.assign(this, {
+      list:
+        (options.emptyList && []) ||
+        Array.prototype.concat.apply(corpus, [options.list || []]),
+      exclude: options.exclude || [],
+      placeHolder: options.placeHolder || '*',
+      regex: options.regex || /[^a-zA-Z0-9|\$|\@]|\^/g,
+      replaceRegex: options.replaceRegex || /\w/g,
+    });
+  }
+
+  isProfane(string) {
+    return (
+      this.list.filter(word => {
+        const wordExp = new RegExp(
+          `\\b${word.replace(/(\W)/g, '\\$1')}\\b`,
+          'gi',
+        );
+        return !this.exclude.includes(word) && wordExp.test(string);
+      }).length > 0 || false
+    );
+  }
+
+  replaceWord(string) {
+    return string
+      .replace(this.regex, '')
+      .replace(this.replaceRegex, this.placeHolder);
+  }
+
+  clean(string) {
+    return string
+      .split(/\b/)
+      .map(word => {
+        return this.isProfane(word) ? this.replaceWord(word) : word;
+      })
+      .join('');
+  }
+
+  addWords() {
+    let words = Array.from(arguments);
+
+    this.list.push(...words);
+
+    words.forEach(word => {
+      if (this.exclude.includes(word)) {
+        this.exclude.splice(this.exclude.indexOf(word), 1);
+      }
+    });
+  }
+
+  removeWords() {
+    this.exclude.push(...Array.from(arguments));
+  }
+}
+
+module.exports = Filter;
